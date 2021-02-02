662ca4dd69f914658827c2b804eec14e7cdcb7b8
WaleedElhussain <wldelhussain@gmail.com>
2021-02-01 23:41:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function start(){
+
+    // $("body").css("background-color", "pink");
+    $("#menu a:first").click();
+
+}
+
+$(window).on("load", start);
+
+
+function loadpage(e){
+
+    e.preventDefault();
+    $("#menu a.active").removeClass("active");
+    $(this).addClass("active");
+
+    var href = $(this).attr("href");    
+    $("#content").load(href);
+
+}
+
+$(document).on("click", "#menu a", loadpage);
